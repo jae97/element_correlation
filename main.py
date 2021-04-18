fe912358c88c80046cea51b8395045046dfc0b8b
jae97 <81499467+jae97@users.noreply.github.com>
2021-04-18 13:51:29 -0700

    Update main.py

--- main.py
+++ main.py
@@ -4,3 +4,4 @@ import pandas as pd
 import time
 
 st.title('Element Correlation')
+st.write("In this mini project, I will be seeing if there is any correlation between one's favorite element and their personality traits.")
