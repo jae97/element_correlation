8187535d00f26f6d1ca0512ac92340b7554d9e50
jae97 <81499467+jae97@users.noreply.github.com>
2021-04-18 14:16:45 -0700

    Update main.py

--- main.py
+++ main.py
@@ -5,3 +5,12 @@ import time
 
 st.title('Element Correlation')
 st.write("In this mini project, I will be seeing if there is any correlation between one's favorite element and their personality traits.")
+
+core_data = pd.DataFrame({
+  'Pyro' : [1, 'ESFP'],
+  'Hydro' : [2, 'ISTP'],
+  'Anemo' : [1, 'INFP'],
+  'Electro' : [2, 'INFP'],
+  'Dendro' : [1, 'INFJ'],
+  'Cryo' : [0],
+  'Geo' : [0]})
