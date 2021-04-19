6258d0571f44abdfc1aa369ca59b61da75d2572f
jae97 <81499467+jae97@users.noreply.github.com>
2021-04-19 11:14:07 -0700

    Update main.py

--- main.py
+++ main.py
@@ -9,14 +9,14 @@ import seaborn as sns
 st.title('Element Correlation')
 st.write("In this mini project, I will be seeing if there is any correlation between one's favorite element and their personality traits.")
 
-core_data = pd.DataFrame({
+core_data = pd.DataFrame([
   'Entry 1' : ['Electro', 'INFP'],
   'Entry 2' : ['Hydro', 'ISTP'],
   'Entry 3' : ['Electro', 'INFP'],
   'Entry 4' : ['Dendro', 'INFJ'],
   'Entry 5' : ['Pyro', 'INFJ'],
   'Entry 6' : ['Anemo', 'INFP'],
-  'Entry 7' : ['Hydro', 'ISTP']})
+  'Entry 7' : ['Hydro', 'ISTP']])
 
 if st.sidebar.checkbox('Show graph'):
   sns.barplot(x=core_data.index, y=core_data['Electro'])
