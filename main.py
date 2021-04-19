91c95092d1ff421b6a6ecb11882fe1f2fda8727e
jae97 <81499467+jae97@users.noreply.github.com>
2021-04-19 11:41:48 -0700

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import pandas as pd
 pd.plotting.register_matplotlib_converters()
 import time
 import seaborn as sns
+import matplotlib.pyplot as plt
 
 st.title('Element Correlation')
 st.write("In this mini project, I will be seeing if there is any correlation between one's favorite element and their personality traits.")
